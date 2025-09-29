2e838a5833c73e1217d28ca042edd2bdbf35d401
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-09-29 11:26:30 -0300

    Removendo arquivos

--- a.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-for proc in psutil.process_iter(['pid', 'name', 'username']):
-    try:
-        print(proc.info)
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
